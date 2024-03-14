7ffb99d6aee9b2eac6927190794dee8b636f23f4
Kelvin Ngan <nth1030@gmail.com>
2024-03-14 23:28:37 +0800

    set up light and dark theme return object

--- react-admin/src/scenes/theme.js
+++ react-admin/src/scenes/theme.js
@@ -121,3 +121,45 @@ export const tokens = (mode) => ({
 });
 
 // mui theme settings
+export const themeSettings = (mode) => {
+    const colors = tokens(mode);
+
+    return  {
+        palette: {
+            mode: mode,
+            ...(mode === 'dark'
+            ? {
+                primary: {
+                    main: colors.primary[500],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: colors.primary[500],
+                }
+            } : {
+                primary: {
+                    main: colors.primary[500],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: "#fcfcfc",
+                }
+            }
+            )
+        }
+    }
+}
